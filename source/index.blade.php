47c325b4fd8e886d2664711edea12a0d116296e3
calibur11 <calibur11@protonmail.com>
2020-01-20 11:07:37 +0100

    test

--- source/index.blade.php
+++ source/index.blade.php
@@ -1,11 +1,11 @@
 @extends('_layouts.master')
 
 @section('content')
-    <h1>Welcome to Artisan Static for Jigsaw</h1>
+    <h1>calibur11 </h1>
 
-    <h2>Asset Compilation Test</h2>
+    <h2>Test/h2>
 
-    <p>CSS test: <span class="test-css">text with border and padding</span></p>
+    <p> <span class="test-css">never look down</span></p>
 
-    <p>JavaScript test: <a href="#" class="test-js">click me</a></p>
+    <p>no , dont do it <a href="#" class="test-js">click me</a></p>
 @endsection
